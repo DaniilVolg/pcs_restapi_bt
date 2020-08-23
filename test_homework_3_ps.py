cc1c8c421cd1ba81b5d438e96f0c53e9a6602403
DaniilVolg <59584072+DaniilVolg@users.noreply.github.com>
2020-08-23 16:12:21 +0300

    Update test_homework_3_ps.py

--- test_homework_3_ps.py
+++ test_homework_3_ps.py
@@ -2,6 +2,8 @@ from time import sleep
 
 import pytest
 
+name = "Bob Boss"
+
 
 @pytest.fixture
 def visit_site(py):
@@ -18,12 +20,21 @@ def login(py):
 def test_successful_enter(py, visit_site, login):
     welcome_text = py.webdriver.find_element_by_id("welcome").text
     assert welcome_text == "Welcome Admin"
-    
 
 
 def test_select_bob_boss(py, visit_site, login):
-    py.get("#empsearch_supervisor_name").type("Bob Boss")
+    py.get("#empsearch_supervisor_name").type(name)
     py.get("#empsearch_job_title").select("QA Lead")
     py.get("#searchBtn").click()
     py.getx("//a[contains(text(),'774863')]").click()
-    sleep(5)
+    page_field = py.get("#wrapper")
+    # resume_field = py.get("#attachmentList")
+    # assert resume_field.contains(text="Attachments")
+    # if page_field.contains(text="resume"):
+    # pass
+    try:
+        assert page_field.contains(text="resume")
+    except:
+        NameError
+    if page_field.should().not_have_text("resume"):
+        print("Mr '{0}' still doesn't have resume".format(name))
